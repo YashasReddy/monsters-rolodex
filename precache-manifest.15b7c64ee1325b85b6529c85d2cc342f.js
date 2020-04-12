07eb13fb2b855d9b90c6bd66d40beae56b4485c4
Yashas Reddy <yashasreddy@Yashass-MacBook-Air.local>
2020-04-12 22:46:11 +0530

    Updates

---
+++ precache-manifest.15b7c64ee1325b85b6529c85d2cc342f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47887639cd96a46be9dc6af44afaef25",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "187c9e59ad5a31756f08",
+    "url": "/monsters-rolodex/static/css/main.94810e39.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters-rolodex/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "187c9e59ad5a31756f08",
+    "url": "/monsters-rolodex/static/js/main.4ecb51e9.chunk.js"
+  },
+  {
+    "revision": "96e4dc8f64b3356b837b",
+    "url": "/monsters-rolodex/static/js/runtime-main.6b89a6eb.js"
+  }
+]);
